fca9c9123dd91064a8041cb1502d0a09f2c26800
joewong1208 <43617400+joewong1208@users.noreply.github.com>
2018-11-29 17:02:50 +0800

    password management update

--- account/urls.py
+++ account/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^doTokenGeneration/', views.doTokenGeneration, name='doTokenGeneration'),
     url(r'^edit_profile/', views.edit_profile, name="edit_profile"),
     url(r'^changePassword/$', views.change_password, name='change_password'),
-	url(r'^password_reset/$', PasswordResetView.as_view(), name='passwordReset'),
+	url(r'^password_reset/$', PasswordResetView.as_view(email_template_name='account/password_reset_email.html', subject_template_name='account/password_reset_subject.txt'), name='passwordReset'),
     url(r'^password_reset/done/$', PasswordResetDoneView.as_view(), name='password_reset_done'),
 	url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     url(r'^reset/done/$', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
